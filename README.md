d1e98df78d4f24548fe37e565218a2c99fa2270f
飞酱 <zjf111435@alibaba-inc.com>
2017-12-26 20:05:11 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,137 @@
+Observable React
+====
+
+在 React 项目中更方便的使用 rx.js
+
+开始使用
+---
+
+```bash
+npm install observable-react
+```
+
+在组件的 `constructor` 中进行初始化：
+
+```javascript
+import setupObservables from 'obserbvable-react';
+
+class Demo extends React.Component {
+    constructor(props) {
+        super(props);
+
+        setupObservables(this);
+    }
+}
+```
+
+
+
+生命周期
+---
+
+初始化之后，生命周期被转化为以下**生命周期流**：
+
+```javascript
+this.componentWillMount$
+this.componentDidMount$
+this.componentWillReceiveProps$
+this.shouldComponentUpdate$
+this.componentWillUpdate$
+this.componentDidUpdate$
+this.componentWillUnmount$
+this.componentDidCatch$
+```
+
+你可以像这样使用：
+
+```javascript
+const clock = Rx.Observable.interval(1000).takeUntil(this.componentWillUnmount$);
+```
+
+你也可以跟以前一样使用 React 的生命周期函数，不冲突。
+
+
+
+快速将 Observables 绑定在 state 上
+---
+
+```javascript
+this.setState$({
+    foo: foo$,
+    bar: bar$,
+});
+```
+
+或者
+
+```javascript
+this.setState$(Rx.Observable.of({
+    foo: 'foo',
+    bar: 'bar',
+}));
+```
+
+组件事件
+---
+
+快速添加事件流
+
+```javascript
+<Button onClick={this.ObservableEvent('buttonClick')} />
+```
+
+调用 `this.ObservableEvent` 会创建一个事件流: this.buttonClick$，在 `componentDidMount` 中可以使用：
+
+```javascript
+this.buttonClick$.subscribe(([event, nativeEvent]) => alert('按钮被点击！'));
+```
+
+由 this.ObservableEvent 创建的事件流默认发送组件的原生事件，你也可以为其添加参数来进行自定义：
+
+- `this.ObservableEvent('buttonClick', (event) => event.target)` 转化组件原生事件
+- `this.ObservableEvent('buttonClick', someData)` 发送自定义数据
+- `this.ObservableEvent('buttonClick', someData, otherData)` 发送多个数据；发送时这些数据会被拼合成一个数组
+
+### 预定义事件
+
+可能有些组件是动态加载的，那么在 `componentDidMount` 中该事件流可能还未定义。可以在 `constructor` 中预先定义事件，以免出现报错。
+
+```javascript
+this.buttonClick$ = new Rx.Subject();
+```
+
+Ajax 请求
+---
+
+从 Ajax 请求快速创建数据流：假设 `fetchRemoteData` 是一个网络请求方法，返回 Promise。
+
+```javascript
+this.remoteData$ = this.createObservableRequest(fetchRemoteData, arg1, arg2);
+```
+
+或者
+
+```
+this.ObservableRequest('remoteData', fetchRemoteData, arg1, arg2);
+```
+
+这样创建的数据流不会立即进行请求，而是有观察者订阅时才会进行发送第一次请求。
+数据流可以进行刷新：
+
+```javascript
+this.remoteData$.request();
+```
+
+假如每10秒刷新一次：
+
+```javascript
+Rx.Observable.interval(10000)
+    .subscribe(() => this.remoteData$.request());
+```
+
+也可以为请求传入新的参数：
+
+```javascript
+this.remoteData$.request(newArg1, newArg2);
+```
+
---
+++ index.js
@@ -0,0 +1,168 @@
+import Rx from 'rxjs';
+
+export default function setupObservables(context) {
+    setupLifeCycles(context);
+    setupEvents(context);
+    setupSetState(context);
+    setupRequests(context);
+}
+
+function setupLifeCycles(context) {
+    const lifeCycles = {
+        componentWillMount: context.componentWillMount,
+        componentDidMount: context.componentDidMount,
+        componentWillReceiveProps: context.componentWillReceiveProps,
+        shouldComponentUpdate: context.shouldComponentUpdate,
+        componentWillUpdate: context.componentWillUpdate,
+        componentDidUpdate: context.componentDidUpdate,
+        componentWillUnmount: context.componentWillUnmount,
+        componentDidCatch: context.componentDidCatch,
+    }
+
+    Object.assign(context, {
+        componentWillMount$: new Rx.Subject(),
+        componentDidMount$: new Rx.Subject(),
+        componentWillReceiveProps$: new Rx.Subject(),
+        shouldComponentUpdate$: new Rx.Subject(),
+        componentWillUpdate$: new Rx.Subject(),
+        componentDidUpdate$: new Rx.Subject(),
+        componentWillUnmount$: new Rx.Subject(),
+        componentDidCatch$: new Rx.Subject(),
+    });
+
+    Object.assign(context, {
+        componentWillMount: () => {
+            context.componentWillMount$.next();
+            return lifeCycles.componentWillMount
+                && lifeCycles.componentWillMount.call(context);
+        },
+
+        componentDidMount: () => {
+            context.componentDidMount$.next();
+            return lifeCycles.componentDidMount
+                && lifeCycles.componentDidMount.call(context);
+        },
+
+        componentWillReceiveProps: nextProps => {
+            context.componentWillReceiveProps$.next(nextProps);
+            return lifeCycles.componentWillReceiveProps
+                && lifeCycles.componentWillReceiveProps.call(context, nextProps);
+        },
+
+        shouldComponentUpdate: (nextProps, nextState) => {
+            context.shouldComponentUpdate$.next({ nextProps, nextState });
+            return lifeCycles.shouldComponentUpdate
+                && lifeCycles.shouldComponentUpdate.call(context, nextProps, nextState);
+        },
+
+        componentWillUpdate: (nextProps, nextState) => {
+            context.componentWillUpdate$.next({ nextProps, nextState });
+            return lifeCycles.componentWillUpdate
+                && lifeCycles.componentWillUpdate.call(context, nextProps, nextState);
+        },
+
+        componentDidUpdate: (nextProps, nextState) => {
+            context.componentDidUpdate$.next({ nextProps, nextState });
+            return lifeCycles.componentDidUpdate
+                && lifeCycles.componentDidUpdate.call(context, nextProps, nextState);
+        },
+
+        componentWillUnmount: () => {
+            context.componentWillUnmount$.next();
+            return lifeCycles.componentWillUnmount
+                && lifeCycles.componentWillUnmount.call(context);
+        },
+
+        componentDidCatch: (error, info) => {
+            context.componentDidCatch$.next({ error, info });
+            return lifeCycles.componentDidCatch
+                && lifeCycles.componentDidCatch.call(context, error, info);
+        },
+    });
+}
+
+function setupEvents(context) {
+    context.ObservableEvent = (name, ...args) => {
+        let observable = context[`${name}$`];
+
+        if (!observable || !isObservable(observable)) {
+            context[`${name}$`] = observable = new Rx.Subject();
+        }
+
+        return (...eventArgs) => {
+            if (args.length > 0) {
+                if (typeof args[0] === 'function') {
+                    return observable.next(valueMapper(...eventArgs));
+                }
+                else if (args.length === 1) {
+                    return observable.next(args[0]);
+                }
+                else {
+                    return observable.next(args);
+                }
+            }
+            else {
+                if (eventArgs.length > 1) {
+                    return observable.next(eventArgs);
+                }
+                else if (eventArgs.length === 1) {
+                    return observable.next(eventArgs[0]);
+                }
+                else {
+                    return observable.next();
+                }
+            }
+        };
+    };
+}
+
+function isObservable(object) {
+    return typeof object === 'object'
+        && typeof object.subscribe === 'function';
+}
+
+function setupSetState(context) {
+    context.setState$ = arg => {
+        if (isObservable(arg)) {
+            arg.subscribe(valuesFromObservable => {
+                if (typeof valuesFromObservable === 'object') {
+                    context.setState(prevState => Object.assign(prevState, valuesFromObservable));
+                }
+            });
+        }
+        else if (typeof arg === 'object') {
+            for (const key in arg) {
+                arg[key].subscribe(value => context.setState({ [key]: value }));
+            }
+        }
+    };
+}
+
+function setupRequests(context) {
+    context.createObservableRequest = (request, ...defaultArgs) => {
+        const request$ = new Rx.BehaviorSubject();
+        const result$ = request$
+            .switchMap((...args) => {
+                const result = args.length
+                    ? request(...args)
+                    : request(...defaultArgs);
+
+                return Rx.Observable.fromPromise(result);
+            });
+
+        result$.request = (...args) => {
+            request$.next(...args);
+
+            return result$;
+        };
+
+        return result$;
+    };
+
+    context.ObservableRequest = (name, request, ...defaultArgs) => {
+        const result$ = context.createObservableRequest(request, ...defaultArgs);
+
+        context[`${name}$`] = result$;
+        return result$;
+    };
+}
